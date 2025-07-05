ffa2b33f4f3568523670005d4d0cf92cb4ee8842
WasinW <104547165+WasinW@users.noreply.github.com>
2025-07-05 17:28:07 +0700

    Read jar path from config or env

--- scripts/airflow/dag/sample_dag.py
+++ scripts/airflow/dag/sample_dag.py
@@ -1,6 +1,25 @@
+"""Sample DAG submitting a Spark job to Dataproc.
+
+The path to the compiled jar can be provided via the FRAMEWORK_JAR
+environment variable or the ``jar_path`` field inside the YAML
+configuration file.
+"""
+
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime
+import os
+import yaml
+
+CONFIG_FILE = os.environ.get(
+    "JOB_CONFIG_FILE",
+    os.path.join(os.path.dirname(__file__), "../config/job_config.yaml"),
+)
+
+with open(CONFIG_FILE) as cfg:
+    config = yaml.safe_load(cfg)
+
+JAR_PATH = os.environ.get("FRAMEWORK_JAR", config.get("jar_path"))
 
 with DAG(
     dag_id="sample_spark_job",
@@ -10,5 +29,5 @@ with DAG(
 ) as dag:
     run_job = BashOperator(
         task_id="run_spark_job",
-        bash_command="gcloud dataproc jobs submit spark --cluster=spark-cluster --jars gs://ntt-test-data-bq-looker-scripts/fw/lib/output/framework.jar --class SimpleJob gs://ntt-test-data-bq-looker-scripts/fw/scripts/SimpleJob.scala",
+        bash_command=f"gcloud dataproc jobs submit spark --cluster=spark-cluster --jars {JAR_PATH} --class SimpleJob gs://ntt-test-data-bq-looker-scripts/fw/scripts/SimpleJob.scala",
     )
