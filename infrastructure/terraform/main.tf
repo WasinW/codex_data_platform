907e58e0682847839ce452c56e31507b02c5739f
WasinW <104547165+WasinW@users.noreply.github.com>
2025-07-05 17:10:41 +0700

    Add node pool and update cluster

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -35,7 +35,21 @@ resource "google_container_cluster" "gke" {
   location = var.region
 
   remove_default_node_pool = true
-  initial_node_count       = 1
+
+  node_config {
+    machine_type = "e2-standard-4"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+}
+
+resource "google_container_node_pool" "gke_nodes" {
+  name       = "airflow-pool"
+  location   = var.region
+  cluster    = google_container_cluster.gke.name
+
+  node_count = 1
 
   node_config {
     machine_type = "e2-standard-4"
